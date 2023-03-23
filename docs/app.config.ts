b1172b5c596b1b0691959ef903b62fe1de9d60c0
Sami Dalouche <sami@narrative.io>
2023-03-23 16:42:39 +0100

    Customize with Narrative-specific info

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'Narrative I/O, Inc Employee Handbook',
+    description: 'The Employee Handbook.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxtstudio',
-      github: 'nuxt-themes/docus'
+      twitter: 'narrative_io',
+      github: 'narrative-io/narrative-handbook'
     },
     aside: {
       level: 0,
@@ -17,7 +17,7 @@ export default defineAppConfig({
     footer: {
       iconLinks: [
         {
-          href: 'https://nuxt.com',
+          href: 'https://www.narrative.io/',
           icon: 'simple-icons:nuxtdotjs'
         }
       ]
